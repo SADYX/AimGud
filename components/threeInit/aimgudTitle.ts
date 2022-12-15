70a0105ef332e66e4372ce9ba156767283de4b52
YX <978345079@qq.com>
2022-12-15 11:52:30 +0800

    通过recoil来进行状态管理，存贮font file

--- components/threeInit/aimgudTitle.ts
+++ components/threeInit/aimgudTitle.ts
@@ -9,6 +9,8 @@ import {
 import * as THREE from "three";
 import { FontLoader } from '@/lib/loaders/FontLoader';
 import { TextGeometry } from '@/lib/geometry/TextGeometry';
+import { fontFileState } from "components/atoms/aimgudModel";
+import { getRecoil, setRecoil } from "recoil-nexus";
 
 type ThreeParams = {
     camera: THREE.PerspectiveCamera;
@@ -23,48 +25,60 @@ const generateLightsFor3D = () => {
     return group;
 }
 
+const generateText = (font: any) => {
+    const group = new THREE.Group();
+    group.name = 'title';
+
+    // A
+    const geo1 = new TextGeometry('A', {
+        font,
+        size: 50,
+        height: 10,
+        curveSegments: 48,
+    });
+    const mat1 = new THREE.MeshPhongMaterial({
+        color: 0x4db244,
+    });
+    const mesh1 = new THREE.Mesh(geo1, mat1);
+    const { max: max1, min: min1 } = new THREE.Box3().expandByObject(mesh1);
+
+    // imGud
+    const geo2 = new TextGeometry('imGud', {
+        font,
+        size: 50,
+        height: 10,
+        curveSegments: 48,
+    });
+    const mat2 = new THREE.MeshLambertMaterial({
+        color: 0xf17f22,
+    });
+    const mesh2 = new THREE.Mesh(geo2, mat2);
+    const { max: max2, min: min2 } = new THREE.Box3().expandByObject(mesh2);
+
+    const length = (max1.x - min1.x) + (max2.x - min2.x);
+    mesh1.position.set(-length / 2, - (max1.y - min1.y) / 2, 0);
+    mesh2.position.set(-length / 2 + (max1.x - min1.x), - (max2.y - min2.y) / 2, 0);
+
+    group.add(mesh1, mesh2);
+    return group;
+}
+
 const addText3D = (scene: THREE.Scene) => {
+
+    const fontFile = getRecoil(fontFileState);
+    if (fontFile) {
+        const text = generateText(fontFile);
+        scene.add(text);
+        return;
+    }
     const loader = new FontLoader();
-    const font = loader.load(
+    loader.load(
         'threeUse/helvetiker_bold.typeface.json',
         (response: any) => {
             const font = response;
-            const group = new THREE.Group();
-            group.name = 'title';
-
-            // A
-            const geo1 = new TextGeometry('A', {
-                font,
-                size: 50,
-                height: 10,
-                curveSegments: 48,
-            });
-            const mat1 = new THREE.MeshPhongMaterial({
-                color: 0x4db244,
-            });
-            const mesh1 = new THREE.Mesh(geo1, mat1);
-            const { max: max1, min: min1 } = new THREE.Box3().expandByObject(mesh1);
-
-            // imGud
-            const geo2 = new TextGeometry('imGud', {
-                font,
-                size: 50,
-                height: 10,
-                curveSegments: 48,
-            });
-            const mat2 = new THREE.MeshLambertMaterial({
-                color: 0xf17f22,
-            });
-            const mesh2 = new THREE.Mesh(geo2, mat2);
-            const { max: max2, min: min2 } = new THREE.Box3().expandByObject(mesh2);
-
-            const length = (max1.x - min1.x) + (max2.x - min2.x);
-            mesh1.position.set(-length / 2, - (max1.y - min1.y) / 2, 0);
-            mesh2.position.set(-length / 2 + (max1.x - min1.x), - (max2.y - min2.y) / 2, 0);
-
-            group.add(mesh1, mesh2);
-
-            scene.add(group);
+            const text = generateText(font);
+            scene.add(text);
+            setRecoil(fontFileState, font);
         }
     );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '../styles/globals.css';
+import { RecoilRoot } from 'recoil';
+import RecoilNexus from 'recoil-nexus';
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <RecoilRoot>
+            <RecoilNexus />
+            <Component {...pageProps} />
+        </RecoilRoot>
+    )
 }
